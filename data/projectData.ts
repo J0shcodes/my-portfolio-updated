8ad87279d09db4f99934bb5027f92cc32fba9174
J0shcodes <mosesjoshua350@gmail.com>
2026-08-23 20:53:17 +0100

    Updating project section

--- data/projectData.ts
+++ data/projectData.ts
@@ -1,28 +1,33 @@
+
 export const projects = [
   {
-    title: "Component Playground",
-    technologies: ["NEXT.JS", "TYPESCRIPT", "JAVASCRIPT", "TAILWINDCSS"],
-    description:
-      "Component Playground is a web app that simplifies React component development by enabling real-time rendering, prop manipulation, and side-by-side code previews. It eliminates the need for local setup, speeds up UI testing, and includes a URL-based sharing feature for easy collaboration and design feedback.",
-    liveUrl: "https://component-playground-beta.vercel.app/",
-    githubUrl: "https://github.com/J0shcodes/component-playground",
+    title: "Lightweight Python API",
+    technologies: [
+      "PYTHON",
+      "PANDAS",
+      "Pydantic",
+      "FASTAPI",
+    ],
+    description: "A lightweight Python REST API built with FastAPI, Pandas, and Pydantic.",
+    githubUrl: "https://github.com/J0shcodes/pandas-query-api"
   },
   {
-    title: "Mr. Clima v2",
-    technologies: ["NEXT.JS", "TYPESCRIPT", "FIREBASE", "TAILWINDCSS"],
+    title: "Lyrtic",
+    technologies: [
+      "NEXT.JS",
+      "TYPESCRIPT",
+      "JAVASCRIPT",
+      "TAILWINDCSS",
+      "CLAUDE API",
+      "NODEJS",
+      "POSTGRESQL",
+    ],
     description:
-      "Mr. Clima v2 is a dynamic and responsive weather forecasting web application built with Next.js, TypeScript, and Tailwind CSS. It fetches real-time weather data from the OpenWeatherMap API and presents it in an intuitive dashboard interface. The app features adaptive background visuals that reflect current weather conditions (like rain, thunder, or snow), along with detailed hourly and daily forecasts formatted for clarity, including 2-hour interval grouping and daily average temperature display.",
-    liveUrl: "https://mr-clima-v2.vercel.app/",
-    githubUrl: "https://github.com/J0shcodes/mr-clima-v2",
+      "Lyrtic is a modern SaaS application that helps small and medium-sized businesses (SMBs) understand their customers through AI-driven insights. It predicts churn risk, analyzes customer behavior, and generates actionable recommendations.",
+    liveUrl: "https://lyrtic.vercel.app/",
+    githubUrl: "https://github.com/J0shcodes/lyrtic",
   },
-  {
-    title: "Joshua Moses Portfolio",
-    technologies: ["NEXT.JS", "TYPESCRIPT", "TAILWINDCSS"],
-    description:
-      "A personal portfolio built to showcase my projects, technical skills, and development journey. Designed with clean UI, responsive layouts, and smooth UX in mind. Built using Next.js, TypeScript, and Tailwind CSS, with a focus on clarity, speed, and storytelling. Features sections for live projects, work in progress, and a dynamic tech stack.",
-    liveUrl: "https://mr-clima-v2.vercel.app/",
-    githubUrl: "https://github.com/J0shcodes/my-portfolio-updated",
-  },
+
   {
     title: "Blindroll",
     technologies: [
@@ -38,12 +43,46 @@ export const projects = [
     liveUrl: "https://blindroll.vercel.app",
     githubUrl: "https://github.com/J0shcodes/blindroll",
   },
+
+  {
+    title: "Mr. Clima v2",
+    technologies: ["NEXT.JS", "TYPESCRIPT", "FIREBASE", "TAILWINDCSS"],
+    description:
+      "Mr. Clima v2 is a dynamic and responsive weather forecasting web application built with Next.js, TypeScript, and Tailwind CSS. It fetches real-time weather data from the OpenWeatherMap API and presents it in an intuitive dashboard interface. The app features adaptive background visuals that reflect current weather conditions (like rain, thunder, or snow), along with detailed hourly and daily forecasts formatted for clarity, including 2-hour interval grouping and daily average temperature display.",
+    liveUrl: "https://mr-clima-v2.vercel.app/",
+    githubUrl: "https://github.com/J0shcodes/mr-clima-v2",
+  },
+
+  {
+    title: "Component Playground",
+    technologies: ["NEXT.JS", "TYPESCRIPT", "JAVASCRIPT", "TAILWINDCSS"],
+    description:
+      "Component Playground is a web app that simplifies React component development by enabling real-time rendering, prop manipulation, and side-by-side code previews. It eliminates the need for local setup, speeds up UI testing, and includes a URL-based sharing feature for easy collaboration and design feedback.",
+    liveUrl: "https://component-playground-beta.vercel.app/",
+    githubUrl: "https://github.com/J0shcodes/component-playground",
+  },
+
+  {
+    title: "Joshua Moses Portfolio",
+    technologies: ["NEXT.JS", "TYPESCRIPT", "TAILWINDCSS"],
+    description:
+      "A personal portfolio built to showcase my projects, technical skills, and development journey. Designed with clean UI, responsive layouts, and smooth UX in mind. Built using Next.js, TypeScript, and Tailwind CSS, with a focus on clarity, speed, and storytelling. Features sections for live projects, work in progress, and a dynamic tech stack.",
+    liveUrl: "https://mr-clima-v2.vercel.app/",
+    githubUrl: "https://github.com/J0shcodes/my-portfolio-updated",
+  },
 ];
 
 export const workInProgress = [
   {
     title: "SemaFy",
-    technologies: ["NEXT.JS", "TYPESCRIPT", "JAVASCRIPT", "TAILWINDCSS", "BLOCKCHAIN", "AI"],
+    technologies: [
+      "NEXT.JS",
+      "TYPESCRIPT",
+      "JAVASCRIPT",
+      "TAILWINDCSS",
+      "BLOCKCHAIN",
+      "AI",
+    ],
     description:
       "semaFy converts smart contracts into clear, human-readable explanations. It helps users understand what a contract does, who controls it, and what risks they take, before interacting on-chain.",
     liveUrl: "https://component-playground-beta.vercel.app/",
